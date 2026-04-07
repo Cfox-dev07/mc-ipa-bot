2fcb2e409b920756a7bdffe1f33bce5323d3c29e
Cfox_dev <113175970+Cfox-dev07@users.noreply.github.com>
2026-04-08 01:17:56 +0900

    Implement file saving from Telegram channel posts
    Add functionality to save files from channel posts based on caption.

--- main.py
+++ main.py
@@ -11,3 +11,25 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
+from telegram.ext import MessageHandler, filters
+
+async def save_file(update, context):
+    if update.channel_post:
+        doc = update.channel_post.document
+        caption = update.channel_post.caption
+
+        if not caption:
+            return
+
+        try:
+            app_name, version = caption.split()
+
+            if app_name.lower() != "minecraft":
+                return
+
+            file_id = doc.file_id
+
+            print("Đã lưu:", version, file_id)
+
+        except:
+            print("Caption sai")
